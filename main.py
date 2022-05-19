9a6dd7c66bf39e997a95d9204aa711e667cd2a13
TheMingus <Magnus.chidester+github@gmail.com>
2022-05-19 15:49:10 +0000

    finished multi file compatibility and added wallpaper grabber

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from os.path import isfile
 from json import load, dump
 import urllib.request
 from tqdm import tqdm
+from urllib.request import urlretrieve
 
 print('Welcome to \u001b[38;2;80;200;120mMingus\'\u001b[0m Chrome Policy tool!')
 print('''Quick Start/First Time Instructions:
@@ -16,20 +17,23 @@ policy = input('\nWhen the file is in place, press enter to continue... ')
 if not listdir('policies'):
     print('\nPolicies directory contains no files. Please add a policy file.')
     quit()
-for i in listdir('policies'):
+policies = listdir('policies')
+del policies[-1:]
+for i in policies:
     if '.json' != i[-5:] and i != '.gitignore':
         print(f'\n{i} is not a JSON file, please remove it.')
         quit()
 
 print('Next, please select an option from the follwing list.\n')
 print('\u001b[96m[0]\u001b[0m Gather Allowed Extension Information\n    Extension IDs, Full Names, and URLs, will be saved to a file')
+print('\u001b[96m[1]\u001b[0m Retrieve Login Wallpaper, and save it to a file')
 print('\n\u001b[93mMore features to come.\u001b[m')
 
 opt = input('\n\u001b[31m> ')
 print('\u001b[0m')
 
 if opt == '0':
-    for i in listdir('policies'):
+    for i in policies:
         with open(f'policies/{i}', 'r') as f:
             policy = load(f)
         extensions = policy.get('chromePolicies').get('ExtensionInstallAllowlist').get('value')
@@ -45,6 +49,12 @@ if opt == '0':
             except urllib.error.HTTPError:
                 results[id] = {'error': True}
                 errors.append(id)
-        # if isfile(f'results/{}'):
-        with open('results/extensions.json', 'w') as f: dump(results, f, indent=2)
+        with open(f'results/extensions_{i}.json', 'w') as f: dump(results, f, indent=2)
         print(f'{len(errors)} extensions were not found. These extensions are no longer on the Chrome Web Store, for various reasons.')
+
+if opt == '1':
+    for i in policies:
+        with open(f'policies/{i}', 'r') as f:
+            policy = load(f)
+        wallpaper = policy.get('chromePolicies').get('DeviceWallpaperImage').get('value').get('url')
+        urlretrieve(wallpaper, f'results/wallpaper_{i}.jpeg')
