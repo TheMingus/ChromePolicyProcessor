66df9c1b33dfeff1d6677d37b1d6bfc65f9cb360
TheMingus <Magnus.chidester+github@gmail.com>
2022-05-19 14:23:09 +0000

    added multiple policy support

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ChromePolicyProcessor
+
+## A (somewhat) easy way to get info from a Chrome Policy File.
+
+---
+
+## Features:
+- Supports multiple policy files (outputs results to seperate files)
+- Lists all extensions that can be installed, including names and URLs to said extensions.
+
+## To Do
+- [x] Speed up extension processing
+- [ ] Save login wallpaper to file
+- [ ] Better support for multiple policy files
--- main.py
+++ main.py
@@ -1,19 +1,28 @@
+from os import listdir
 from os.path import isfile
 from json import load, dump
 import urllib.request
 from tqdm import tqdm
 
 print('Welcome to \u001b[38;2;80;200;120mMingus\'\u001b[0m Chrome Policy tool!')
-print('To begin, please do the following:\n> Open chrome://policy\n> Select \u001b[1mExport to JSON\u001b[0m\n> Place the file in the same directory as this script.')
+print('''Quick Start/First Time Instructions:
+> Open chrome://policy
+> Select \u001b[1mExport to JSON\u001b[0m
+> Place the file in the policies directory.
+
+> If you would like to process more than one policy, place multiple files in the policies directory.''')
 policy = input('\nWhen the file is in place, press enter to continue...')
 
-if isfile('policies.json'):
-    policy = 'policies.json'
-elif not isfile('policies.json'):
-    while not isfile(policy):
-        print('File was not found in the script directory. Please input the policy file\'s path')
-        policy = input('\n> ')
-with open(policy, 'r') as f: policy = load(f)
+# if isfile('policies.json'):
+#     policy = 'policies.json'
+# elif not isfile('policies.json'):
+#     while not isfile(policy):
+#         print('File was not found in the script directory. Please input the policy file\'s path')
+#         policy = input('\n> ')
+for i in listdir('policies'):
+    if '.json' != i[-5:]:
+        print(f'{i} is not a JSON file, please remove it.')
+        quit()
 
 print('Next, please select an option from the follwing list.\n')
 print('\u001b[96m[0]\u001b[0m Gather Allowed Extension Information\n    Extension IDs, Full Names, and URLs, will be saved to a file')
@@ -25,7 +34,7 @@ print('\u001b[0m')
 if opt == '0':
     extensions = policy.get('chromePolicies').get('ExtensionInstallAllowlist').get('value')
     results = {}
-    errors = 0
+    errors = []
     for id in tqdm(extensions):
         try:
             url = urllib.request.urlopen(f'https://chrome.google.com/webstore/detail/{id}').geturl()
@@ -35,6 +44,7 @@ if opt == '0':
             results[id] = {'error': False, 'name': name, 'url': url}
         except urllib.error.HTTPError:
             results[id] = {'error': True}
-            errors += 1
-    with open('results.json', 'w') as f: dump(results, f, indent=2)
-    print(f'{errors} extensions were not found. These extensions are no longer on the Chrome Web Store, for various reasons.')
+            errors.append(id)
+    # if isfile(f'results/{}'):
+    with open('results/extensions.json', 'w') as f: dump(results, f, indent=2)
+    print(f'{len(errors)} extensions were not found. These extensions are no longer on the Chrome Web Store, for various reasons.')
